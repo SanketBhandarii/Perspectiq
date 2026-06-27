9cc7ba9430169570833edc90264f8def89733c17
SanketBhandarii <bhandarisanketp@gmail.com>
2026-06-27 20:26:49 +0530

    fix: update CORS for whop iframe support

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,8 @@ origins = [
     "https://perspecti.vercel.app",
     "https://perspecti.vercel.app/",
     "http://127.0.0.1:3000",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "https://whop.com",
 ]
 
 app.add_middleware(
